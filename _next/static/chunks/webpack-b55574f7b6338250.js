6e6d6849e2cb8572faf53880135c2078ddabb59b
Thankstante <thankstante@gmail.com>
2022-12-11 07:08:55 +0000

    Deploy gh-pages

--- _next/static/chunks/webpack-b55574f7b6338250.js
+++ _next/static/chunks/webpack-b55574f7b6338250.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "37ae04c3c8fa0ad3" + ".css";
+/******/ 			return "static/css/" + "e59904e35c8e18a1" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
